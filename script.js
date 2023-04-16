f9f0a4d29715bf4ee14b419c07a27bb33fe63939
monique <moniquejp16@gmail.com>
2023-04-16 14:04:24 -0300

    teste final 1.0

--- script.js
+++ script.js
@@ -5,30 +5,38 @@ axios.defaults.headers.common['Authorization'] = '2WykYIFo1nfqpAeCvmGSo87Z';
 console.log(axios);
 
 let listaDePessoas = [];
-let listaDeMensagem = [];
+
 let verificar = 0;
-let mensagem , mensagemCom, aparecer;
+let mensagem, aparecer;
+let mensagemCom = {
+    from: '', to: "Todos", text: '', type: "message"
+}
 let nome;
+let resposta, conferindo;
+let adicionar = { name: '', };
 
 perguntar();
-function perguntar(){
-// perguntoando o nome
- nome = prompt('Qual o seu nome?');
-// adicionando o nome em um objeto
-let adicionar = {
-    name: nome,
-};
-// adicionando o nome da pessoa em um array, mais acho que nao precisa
-listaDePessoas.push(adicionar);
-// mandando o nome da pessa pro servidor
-let resposta = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants ', adicionar);
-resposta.then(ver);
-resposta.catch(naoDeu);
-};
+function perguntar() {
+    // perguntoando o nome
+    nome = prompt('Qual o seu nome?');
+    // adicionando o nome em um objeto
+    adicionar.name = nome;
+    mensagemCom.from = nome;
 
+    // mandando o nome da pessa pro servidor
+    resposta = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants ', adicionar);
+    resposta.then(ver);
+    resposta.catch(naoDeu);
+};
+function timestatus() {
+    conferindo = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', adicionar);
+}
 // vendo se deu eraado ou certo
 function ver(olhar) {
     console.log(`deu certo ${olhar.status}`);
+    setInterval(timestatus, 5000);
+    atualizarMensagem();
+    setInterval(atualizarMensagem, 5000);
 
 };
 function naoDeu(rep) {
@@ -36,35 +44,61 @@ function naoDeu(rep) {
     perguntar();
 };
 
- //fazendo o botão de envio fincionar
+//fazendo o botão de envio fincionar
 function enviarMensagem() {
-
     // salvando o que a pessoa digitou
     mensagem = document.getElementById('digitar').value;
-    // salvando o horario que a pessoa digitou a mensagem
-    const data = new Date();
-    console.log(mensagem);
-
-    // enviando a menagem para o servidor do geito que pediram
-    mensagemCom = {
-        from: nome,
-        to: "nome do destinatário (Todos se não for um específico)",
-        text: mensagem,
-        type: "message" // ou "private_message" para o bônus
-    }
+    mensagemCom.text = mensagem;
     console.log(mensagemCom);
-    let mensagemCompleta = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', mensagemCom)
-    console.log(mensagemCompleta);}
+    // salvando o horario que a pessoa digitou a mensagem
+    // enviando a menagem para o servidor do geito que pediram
+    let mensagemCompleta = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', mensagemCom);
+    mensagemCompleta.then((respostas)=>{
+        console.log(respostas);
+        atualizarMensagem();
+    });
+    mensagemCompleta.catch(saiuDaSala);
+   
+}
 
-    // preciso pegar a mensagem digitada o nome e o horario e mostrar na tela, 
+function saiuDaSala(){
+    window.location.reload();
+};
 
-   /* for (let i = 0; i < .length; i++) {
-        listaDeMensagem[i] =
-            `<div class="pessoas">
-                <p>`${data}`</p>
-                <p>`${nome}`</p>
-                <p>`${mensagem}`</p>
-            </div>`
-    }
+// preciso pegar a mensagem digitada o nome e o horario e mostrar na tela, 
+function atualizarMensagem(){
+let verMensagemDoServidor = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+verMensagemDoServidor.then(listaDeMensagem);
+};
+function listaDeMensagem(resposta) {
 
-}*/
+    let buscar = document.querySelector('.interaçao');
+    buscar.innerHTML = '';
+    
+    for (let i = 0; i < resposta.data.length; i++){
+        if (resposta.data[i].type === 'status') {
+            buscar.innerHTML += `<div class="pessoas status">
+             <p> <span class = "timer"> ${resposta.data[i].time} 
+             </span> <span class = "negrito"> ${resposta.data[i].from}
+             </span> ${resposta.data[i].text}</p>
+         </div>`
+        } else if (resposta.data[i].type === 'message'){
+            buscar.innerHTML += `<div class="pessoas message">
+             <p> <span class = "timer"> ${resposta.data[i].time} 
+             </span> <span class = "negrito"> ${resposta.data[i].from}
+             </span> para <span class = "negrito"> ${resposta.data[i].to} 
+             </span> ${resposta.data[i].text}</p>
+         </div>`
+        } else if(resposta.data[i].type === 'private_message' &&
+        ((resposta.data[i].from === adicionar.name) || 
+        (resposta.data[i].to === adicionar.name))
+        ){
+            buscar.innerHTML += `<div class="pessoas privado">
+             <p> <span class = "timer"> ${resposta.data[i].time} 
+             </span> <span class = "negrito"> ${resposta.data[i].from}
+             </span> reservdamente para <span class = "negrito"> ${resposta.data[i].to} 
+             </span> ${resposta.data[i].text}</p>
+         </div>`
+        };
+    };
+} ;
